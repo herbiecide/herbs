6757f630fbc9a0251455d8ef68c00d3ff933a076
herbiecide <60129131+herbiecide@users.noreply.github.com>
2020-01-24 19:49:25 -0500

    Add files via upload
    Resizes images in photoshop to have a maximum dimension of 2048 pixels.

---
+++ Resize2048.py
@@ -0,0 +1,48 @@
+# This script resizes images to have a longest edge of 2048 pixels
+# 2048 pixels is the maximum dimension for Facebook, Instagram, and Twitter.
+
+from win32com.client import Dispatch, GetActiveObject
+import os
+
+
+# Start up Photoshop application
+# Or get Reference to already running Photoshop application instance
+# app = Dispatch('Photoshop.Application')
+app = GetActiveObject("Photoshop.Application")
+
+# Save current settings
+ogRulerUnits = app.Preferences.RulerUnits
+ogTypeUnits = app.Preferences.TypeUnits
+
+# Set to pixels
+app.Preferences.RulerUnits = 1 #1=pixels
+app.Preferences.TypeUnits = 1 #1=pixels
+
+maxDim=2048 # Maximum dimension for web size
+
+if len(app.Documents) > 0:
+
+        doc = app.ActiveDocument
+        imageHeight=doc.height
+        imageWidth=doc.width
+
+        # Check image is bigger than desired web size
+        if (imageWidth>maxDim) and (imageHeight>maxDim):
+            # Check which side is longer and resize accordingly
+            if(imageWidth>=imageHeight):
+
+                doc.ResizeImage(maxDim,None, None,None)
+
+            else:
+                doc.ResizeImage(None, maxDim, None,None)
+        else:
+            print("No need to resize. Image is too small")
+
+else:
+    print("You must have at least one open document to run this script!")
+
+
+
+# set the app preference the way it was before the operation
+app.Preferences.RulerUnits = ogRulerUnits
+app.Preferences.TypeUnits = ogTypeUnits
